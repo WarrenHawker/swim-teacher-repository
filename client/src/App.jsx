5b0a58965cfaf7a6ed92a0f7c8327ee6962bc5b0
WarrenHawker <hawker.warren@gmail.com>
2022-12-14 11:26:42 +0000

    added react-router to client and styled header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/header';
 import Dashboard from './pages/dashboard';
 import Home from './pages/home';
@@ -6,7 +7,26 @@ import Signup from './pages/signup';
 import SingleEntry from './pages/singleEntry';
 
 const App = () => {
-  return <></>;
+  const user = null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route
+            path='/login'
+            element={!user ? <Login /> : <Navigate to='/dashboard' />}></Route>
+          <Route
+            path='/signup'
+            element={!user ? <Signup /> : <Navigate to='/dashboard' />}></Route>
+          <Route
+            path='/dashboard'
+            element={user ? <Dashboard /> : <Navigate to='/login' />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,8 +1,28 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
+  const user = null;
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <header>
+      <Link to='/'>
+        <h2>SwimHub</h2>
+      </Link>
+
+      {!user ? (
+        <nav>
+          <Link to='/login' className='active'>
+            Login
+          </Link>
+          <Link to='/signup'>Signup</Link>
+        </nav>
+      ) : (
+        <nav>
+          {/* dashboard link shows user name */}
+          <Link to='/dashboard'>Dashboard</Link>
+          <Link to='/'>Logout</Link>
+        </nav>
+      )}
+    </header>
   );
 };
 
