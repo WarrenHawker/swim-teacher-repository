24da77a08d7a3d6fd568272887dd62366123ca4a
WarrenHawker <hawker.warren@gmail.com>
2022-12-14 12:16:51 +0000

    styled login form

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,24 +4,24 @@ const Header = () => {
   const user = null;
   return (
     <header>
-      <Link to='/'>
-        <h2>SwimHub</h2>
-      </Link>
+      <div className='header-inner'>
+        <Link to='/'>
+          <h2>SwimHub</h2>
+        </Link>
 
-      {!user ? (
-        <nav>
-          <Link to='/login' className='active'>
-            Login
-          </Link>
-          <Link to='/signup'>Signup</Link>
-        </nav>
-      ) : (
-        <nav>
-          {/* dashboard link shows user name */}
-          <Link to='/dashboard'>Dashboard</Link>
-          <Link to='/'>Logout</Link>
-        </nav>
-      )}
+        {!user ? (
+          <nav>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+          </nav>
+        ) : (
+          <nav>
+            {/* dashboard link shows user name */}
+            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/'>Logout</Link>
+          </nav>
+        )}
+      </div>
     </header>
   );
 };
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,8 +1,36 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const formSubmitHandler = () => {};
+
   return (
-    <>
-      <h1>Login page</h1>
-    </>
+    <form onSubmit={formSubmitHandler}>
+      <label htmlFor='email'>Email</label>
+      <input
+        type='email'
+        name='email'
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+
+      <label htmlFor='password'>Password</label>
+      <input
+        type='password'
+        name='password'
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <p className='error'>error message</p>
+      <button className='btn btn-primary'>Login</button>
+    </form>
   );
 };
 
